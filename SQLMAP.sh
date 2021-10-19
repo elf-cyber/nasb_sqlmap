a906b4957aca4ffef40817fe6f9309a15b649b70
elf <87319528+elf-cyber@users.noreply.github.com>
2021-10-19 18:20:01 +0330

    Add files via upload

---
+++ SQLMAP.sh
@@ -0,0 +1,21 @@
+#MY ID:E_L_F_6_6_6
+#!\bash
+echo "[+]SABRE KONID..!"
+sleep(1)
+pkg install git
+clear
+echo "[-]DOBARE SABER KONID...!"
+sleep(1)
+pkh install python
+clear
+echo "[+]DARAM BARAT NASB MIKONAM SQLMAP RO..!"
+ECHO "[-]SABRE KOID....!"
+sleep(1)
+git clone https://github.com/sqlmapproject/sqlmap
+clear
+cd sqlmap
+
+echo "[$]SQLMAP NASB SHOD..! "
+echo "[#]RUN..? (ENTER THE TERMINAL: ==> python sqlmap.py)"
+echo "[&]BY KOS MADARET[$]"
+echo "[%]CREATED BY: @ELF_SECURITY_CYBER"
